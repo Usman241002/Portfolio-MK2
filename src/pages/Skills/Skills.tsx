125de4de60f57394774e401b75f8ec6ca2ba8beb
Usman Khalid <ukhalid428@gmail.com>
2025-05-28 23:56:34 +0100

    Fixed bugs with images not loading as they were in the src folder

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -2,58 +2,63 @@ import SkillsCard from "../../components/SkillsCard/SkillsCard";
 import "./Skills.css";
 
 export default function Skills() {
-  const basePath = "../../src/images/skillIcons";
+  const basePath = "images/skillIcons";
 
   const skills = [
     {
       skillName: "HTML5",
-      imgPath: "/html.svg"
+      imgPath: "/html.svg",
     },
     {
       skillName: "CSS",
-      imgPath: "/css.svg"
+      imgPath: "/css.svg",
     },
     {
       skillName: "JavaScript",
-      imgPath: "/JavaScript.svg"
+      imgPath: "/JavaScript.svg",
     },
     {
       skillName: "PHP",
-      imgPath: "/Php.svg"
+      imgPath: "/Php.svg",
     },
     {
       skillName: "C",
-      imgPath: "/C.svg"
+      imgPath: "/C.svg",
     },
     {
       skillName: "C++",
-      imgPath: "/C++.svg"
+      imgPath: "/C++.svg",
     },
     {
       skillName: "Python",
-      imgPath: "/Python.svg"
+      imgPath: "/Python.svg",
     },
     {
       skillName: "React",
-      imgPath: "/React.svg"
+      imgPath: "/React.svg",
     },
     {
       skillName: "NodeJS",
-      imgPath: "/NodeJs.svg"
+      imgPath: "/NodeJs.svg",
     },
     {
       skillName: "PostgreSQL",
-      imgPath: "/Postgre.svg"
-    }
-  ]
+      imgPath: "/Postgre.svg",
+    },
+  ];
 
-  return(
+  return (
     <main id="skill-subpage" className="about-sub-body">
       <h3>Skills</h3>
 
       <div id="skills">
-        {skills.map(skill => (<SkillsCard skillName={skill.skillName} imgPath={basePath + skill.imgPath}/>))}
+        {skills.map((skill) => (
+          <SkillsCard
+            skillName={skill.skillName}
+            imgPath={basePath + skill.imgPath}
+          />
+        ))}
       </div>
     </main>
-  )
+  );
 }
