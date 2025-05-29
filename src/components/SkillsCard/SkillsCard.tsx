db3c914df382fa25165ad39e89d2ef117948abc9
Usman Khalid <ukhalid428@gmail.com>
2025-05-29 15:22:54 +0100

    Bug fixes to do with skills icons not displaying

--- src/components/SkillsCard/SkillsCard.tsx
+++ src/components/SkillsCard/SkillsCard.tsx
@@ -3,13 +3,15 @@ import "./SkillsCard.css";
 type props = {
   skillName: string;
   imgPath: string;
-}
+};
 
-export default function SkillsCard({skillName, imgPath}: props) {
-  return(
+export default function SkillsCard({ skillName, imgPath }: props) {
+  const basePath = "/images/skillIcons/";
+
+  return (
     <section className="skill-card">
-      <img src={imgPath} />
+      <img src={basePath + imgPath} />
       <p>{skillName}</p>
     </section>
-  )
+  );
 }
--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -2,46 +2,54 @@ import SkillsCard from "../../components/SkillsCard/SkillsCard";
 import "./Skills.css";
 
 export default function Skills() {
-  const basePath = "images/skillIcons/";
-
   const skills = [
     {
+      id: 1,
       skillName: "HTML5",
       imgPath: "html.svg",
     },
     {
+      id: 2,
       skillName: "CSS",
       imgPath: "css.svg",
     },
     {
+      id: 3,
       skillName: "JavaScript",
       imgPath: "JavaScript.svg",
     },
     {
+      id: 4,
       skillName: "PHP",
       imgPath: "Php.svg",
     },
     {
+      id: 5,
       skillName: "C",
       imgPath: "C.svg",
     },
     {
+      id: 6,
       skillName: "C++",
       imgPath: "C++.svg",
     },
     {
+      id: 7,
       skillName: "Python",
       imgPath: "Python.svg",
     },
     {
+      id: 8,
       skillName: "React",
       imgPath: "React.svg",
     },
     {
+      id: 9,
       skillName: "NodeJS",
       imgPath: "NodeJs.svg",
     },
     {
+      id: 10,
       skillName: "PostgreSQL",
       imgPath: "Postgre.svg",
     },
@@ -54,8 +62,9 @@ export default function Skills() {
       <div id="skills">
         {skills.map((skill) => (
           <SkillsCard
+            key={skill.id}
             skillName={skill.skillName}
-            imgPath={basePath + skill.imgPath}
+            imgPath={skill.imgPath}
           />
         ))}
       </div>
